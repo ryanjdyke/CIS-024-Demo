68916a4cbce441db616df828b59d7a7d026db0f0
Ryan Dyke <ryanjdyke@gmail.com>
2023-10-12 15:21:30 -0700

    Add week 1 homework

---
+++ week_1_hw.py
@@ -0,0 +1,15 @@
+# Put down your name
+my_name = "Ryan Dyke"
+# Put down the course name you're working on
+course_name = "Introduction to Python"
+# Current month (in integer please!)
+curr_month = 9 # change this to ..?
+
+print(f"Hello {my_name}! The course ends on Dec, which is {12-curr_month} months from now.")
+print("Hope you have a great time learning this course")
+
+today = 86.7
+yesterday = 87.0
+# print out the average temperature of today and yesterday
+avg_temp = (today + yesterday) / 2 # fill in here!
+print(f"average temperature is {avg_temp}")
